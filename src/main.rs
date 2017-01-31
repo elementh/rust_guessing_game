1c540c1b54d21bf2263a595716c5135f55d804f9
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2017-01-31 23:45:56 +0100

    Added random number generation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     println!("Please enter your guess:");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     let mut guess = String::new();
+
+    println!("The secret number is: {}", secret_number);
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
