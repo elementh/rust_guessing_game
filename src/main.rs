b880313b8c45ba54ef72a5ade6288436268f8c2a
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2017-02-13 14:26:06 +0100

    Added type Guess and panic if number > 100 or number < 1

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,24 @@ use rand::Rng;
 fn main() {
     println!("Guess the number between 1 and 100!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = Guess::new(rand::thread_rng().gen_range(1, 101));
 
+    struct Guess {
+        value: u32,
+    }
+    impl Guess {
+        pub fn new(value: u32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}.", value);
+            }
+            Guess {
+                value: value,
+            }
+        }
+        pub fn value(&self) -> u32 {
+            self.value
+        }
+    }
     //    println!("The secret number is: {}", secret_number);
 
     loop {
@@ -18,21 +34,18 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line!");
 
-        print!("You guessed: {}.", guess);
+        print!("You guessed: {}, ", guess.trim());
 
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
-        if guess < 1 || guess > 100 {
-            print!("Which is not a number between 1 and 100. ");
-            continue
-        }
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+        match guess.value().cmp(&secret_number.value()) {
+            Ordering::Less => println!("which is too small!"),
+            Ordering::Greater => println!("which is too big!"),
             Ordering::Equal => {
+                println!("which is right!");
                 println!("You win!");
                 break;
             },
